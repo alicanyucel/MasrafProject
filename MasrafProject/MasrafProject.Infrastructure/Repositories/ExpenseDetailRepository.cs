4c60833e7476f683084689f600bcac4105395359
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-10-17 21:12:59 +0300

    ExpenseDetail

---
+++ MasrafProject/MasrafProject.Infrastructure/Repositories/ExpenseDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using MasrafProject.Domain.Entities;
+using MasrafProject.Domain.Repositories;
+using MasrafProject.Infrastructure.Context;
+
+namespace MasrafProject.Infrastructure.Repositories;
+
+internal sealed class ExpenseDetailRepository : Repository<ExpenseDetail, ApplicationDbContext>, IExpenseDetailRepository
+{
+    public ExpenseDetailRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
