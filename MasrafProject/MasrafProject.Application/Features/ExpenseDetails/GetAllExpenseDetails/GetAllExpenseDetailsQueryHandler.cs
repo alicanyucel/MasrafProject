130222328b51a028ef75e3818d4904a90056268c
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-10-17 21:41:50 +0300

    getall

--- MasrafProject/MasrafProject.Application/Features/ExpenseDetails/GetAllExpenseDetails/GetAllExpenseDetailsQueryHandler.cs
+++ MasrafProject/MasrafProject.Application/Features/ExpenseDetails/GetAllExpenseDetails/GetAllExpenseDetailsQueryHandler.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MasrafProject.Domain.Entities;
+using MasrafProject.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
 
-namespace MasrafProject.Application.Features.ExpenseDetails.GetAllExpenseDetails
+namespace MasrafProject.Application.Features.ExpenseDetails.GetAllExpenseDetails;
+
+internal sealed class GetAllExpenseDetailQueryHandler : IRequestHandler<GetAllExpenseDetailsQuery, Result<List<ExpenseDetail>>>
 {
-    internal class GetAllExpenseDetailsQueryHandler
+    private readonly IExpenseDetailRepository _expenseDetailRepository;
+
+    public GetAllExpenseDetailQueryHandler(IExpenseDetailRepository expenseDetailRepository)
     {
+        _expenseDetailRepository = expenseDetailRepository;
+    }
+
+    public async Task<Result<List<ExpenseDetail>>> Handle(GetAllExpenseDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var expenseDetail = await _expenseDetailRepository
+            .GetAll()
+            .Where(x => !x.IsDeleted)
+            .ToListAsync(cancellationToken);
+        return Result<List<ExpenseDetail>>.Succeed(expenseDetail);
     }
 }
